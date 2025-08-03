9ec65362f541f4bd78e61a82b423576b62c0a03f
xbaun <xbaunis@gmail.com>
2025-08-03 19:46:29 +0200

    feat: add Extension.svg

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,12 +1,12 @@
 <?php
 
-$EM_CONF['captchafox_official'] = [
+$EM_CONF[$_EXTKEY] = [
     'title' => 'captchafox_official',
     'description' => 'CaptchaFox Typo3',
     'category' => 'fe',
     'author_company' => 'scorialabs',
     'state' => 'stable',
-    'version' => '10.0.0',
+    'version' => '10.0.1',
     'constraints' => [
         'depends' => [
             'typo3' => '10.0.0-11.9.99',
